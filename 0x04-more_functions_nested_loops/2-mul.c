e83b782fb633dfa6b3d007c769d994db5beea1c2
Mogaito <mogaitoisaac@gmail.com>
2023-01-01 04:09:00 -0800

    from task 0-12

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - multiple two integers
+ * @a: first integer
+ * @b: second integer
+ * Code by Isaac Mogaito
+ * Return: addition of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
